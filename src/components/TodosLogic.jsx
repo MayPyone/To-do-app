b6ef1a61a00481c90409379265ea5e00b09a8ebc
May Pyone <maypyone015@gmail.com>
2023-08-30 11:08:56 +0630

    Add delete function and toggle checkbox

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,7 +1,8 @@
 import InputTodo from '@/components/InputTodo';
 import TodosList from '@/components/TodosList';
+import { useState } from 'react';
 const TodosLogic = () => {
-    const todos = [
+    const [todos, setTodos] = useState([
         {
           id: 1,
           title: 'Setup development environment',
@@ -17,11 +18,31 @@ const TodosLogic = () => {
           title: 'Deploy to live server',
           completed: false,
         },
-      ];
+      ])
+       const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
     return (
         <div>
         <InputTodo />
-        <TodosList todosProps={todos} />
+        <TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo} />
       </div>
         
     )
