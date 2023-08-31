aca1d8bd173c9bf485cb187b895855569954e72a
May Pyone <maypyone015@gmail.com>
2023-08-31 14:44:16 +0630

    Set up Linters

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
-
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
